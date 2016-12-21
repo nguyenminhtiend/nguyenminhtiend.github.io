82def7270afdc06ca8455cc94094bc49fc0a9d75
nguyenminhtiend <nguyenminhtiend@gmail.com>
2016-12-21 21:40:00 +0700

    #

--- js/app.config.js
+++ js/app.config.js
@@ -1,1 +1,1 @@
-!function(t){t.env=t.env||{},t.env.apiUrl="http://ec2-52-77-214-252.ap-southeast-1.compute.amazonaws.com"}(this);
+!function(n){n.env=n.env||{},n.env.apiUrl="http://localhost:54516/"}(this);
