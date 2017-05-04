0ca0c1440ff4c402ed481af67e7146a99e883fa7
soniaoh74 <sonia.oh@2359media.com>
2017-05-04 17:05:14 +0800

    test

--- Ethoz/js/config.js
+++ Ethoz/js/config.js
@@ -1,5 +1,5 @@
 (function (window) {
     window.env = window.env || {};
-    //window.env.apiUrl = 'http://localhost:62898';
+   // window.env.apiUrl = 'http://localhost:62898';
   	window.env.apiUrl = 'http://ethoz-webapi-sandbox.azurewebsites.net';
 })(this);
