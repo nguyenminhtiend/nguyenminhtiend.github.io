285de9eb18f422b807ed8def130353c472d4ec20
Tien Nguyen <nguyenminhtiend@gmail.com>
2017-08-22 14:18:33 +0700

    #

--- Ethoz/js/config.js
+++ Ethoz/js/config.js
@@ -2,4 +2,5 @@
     window.env = window.env || {};
     //window.env.apiUrl = 'http://localhost:62898';
   	window.env.apiUrl = 'http://ethoz-webapi-sandbox.azurewebsites.net';
+    //window.env.apiUrl = 'http://controllerdashboard-uat.azurewebsites.net/';
 })(this);
